1aef9205d910c7d3d8a121e6ae0840563f4bf8c7
Stefano Frontini <stefanofrontini@MacAir.lan>
2023-05-21 17:27:32 +0200

    dev environment files

---
+++ .config/nvim/lua/stefano/plugins/lsp/lspsaga.lua
@@ -0,0 +1,34 @@
+-- import lspsaga safely
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+	return
+end
+
+saga.setup({
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+	-- use enter to open file with definition preview
+	definition = {
+		edit = "<CR>",
+	},
+	finder = {
+		max_height = 0.5,
+		min_width = 30,
+		force_max_height = false,
+		keys = {
+			jump_to = "p",
+			expand_or_jump = "<CR>",
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			tabnew = "r",
+			quit = { "q", "<ESC>" },
+			close_in_preview = "<ESC>",
+		},
+	},
+	ui = {
+		colors = {
+			normal_bg = "#022746",
+		},
+	},
+})
---
+++ README.md
@@ -0,0 +1,1 @@
+Dev Envirnment Files
